ec73daa9c2767dc5ab489bec31ca62800f9c36fc
ChrisPittman <51276178+ChrisPittman343@users.noreply.github.com>
2021-04-05 00:21:50 -0400

    Set up environment variables

--- services/firebase.js
+++ services/firebase.js
@@ -3,24 +3,14 @@ import "firebase/auth";
 import "firebase/database";
 import "firebase/functions";
 
-// const firebaseConfig = {
-//   apiKey: process.env.FB_API_KEY,
-//   authDomain: process.env.FB_AUTH_DOMAIN,
-//   projectId: process.env.FB_PROJECT_ID,
-//   storageBucket: process.env.FB_STORAGE_BUCKET,
-//   messagingSenderId: process.env.FB_MESSAGING_SENDER_ID,
-//   appId: process.env.FB_APP_ID,
-//   measurementId: process.env.FB_MEASUREMENT_ID,
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyDhIT3vvoYJyFPIksdGraJT0J-umQYY8xo",
-  authDomain: "sproos-a1282.firebaseapp.com",
-  projectId: "sproos-a1282",
-  storageBucket: "sproos-a1282.appspot.com",
-  messagingSenderId: "754047213842",
-  appId: "1:754047213842:web:cf00ce690267c6984553cb",
-  measurementId: "G-7JGD8EDG9X",
+  apiKey: process.env.NEXT_PUBLIC_FB_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FB_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FB_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FB_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FB_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FB_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FB_MEASUREMENT_ID,
 };
 
 firebase.apps.length === 0
